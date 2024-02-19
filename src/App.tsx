bb882e2a91cee13e9ff54fb24799392d9b428b3b
germanp007 <pintogerman281@gmail.com>
2024-02-19 19:00:05 -0300

    confg grid

--- src/App.tsx
+++ src/App.tsx
@@ -38,18 +38,18 @@ const App = () => {
     //   <div className="bg-teal-600  col-span-3 row-span-5 row-start-2">4</div>
     // </div>
     <main
-      className=" w-screen min-h-screen flex flex-col font-josefinSans md:h-[800px] md:grid md:grid-cols-12 md:grid-rows-6 "
+      className=" w-screen min-h-screen flex flex-col font-josefinSans md:h-[800px] md:grid md:grid-cols-12 md:grid-rows-12 "
       style={{
         backgroundImage:
           windowWidth >= 768 ? "url('/images/bg-pattern-desktop.svg')" : "",
       }}
     >
-      <picture className="h-[86px] flex justify-start items-center ml-7 md:col-span-7">
+      <picture className="h-[86px] flex justify-start items-center ml-7 md:col-span-7 md:row-span-3">
         <img src="../images/logo.svg" alt="logo" className="w-[100px] " />
       </picture>
       {windowWidth >= 768 ? (
         <aside
-          className="md:col-span-5 md:row-span-6"
+          className="md:col-span-5 md:row-span-12"
           style={{
             backgroundImage: "url('../images/hero-desktop.jpg')",
             backgroundRepeat: "no-repeat",
@@ -66,15 +66,15 @@ const App = () => {
         </picture>
       )}
 
-      <article className="flex flex-col gap-8  justify-center items-center leading-[1.35rem] h-[400px] md:col-span-3">
-        <h1 className="text-DarkGrayishRed uppercase text-[38.4px] text-center w-[315px] h-[120px] tracking-[1rem] font-semibold leading-[2.63rem]">
+      <article className="flex flex-col gap-8 mt-6 justify-center items-center leading-[1.35rem] min-h-[400px]  md:col-span-7 md:row-span-10 row-start-4">
+        <h1 className="text-DarkGrayishRed uppercase text-[38.4px] text-center w-[315px] min-h-[120px] tracking-[1rem] font-semibold leading-[2.63rem] md:text-[63px] md:leading-[4.63rem] md:text-start">
           {" "}
           <span className="text-DesaturatedRed font-extralight">
             We're
           </span>{" "}
           coming soon
         </h1>
-        <p className="text-[14.4px] text-DesaturatedRed w-[315px] text-center font-light tracking-[0.028rem]">
+        <p className="text-[14.4px] text-DesaturatedRed w-[315px] text-center font-light tracking-[0.028rem] md:text-[16px] md:text-left md:w-[400px]">
           Hello fellow shoppers! We're currently building our new fashion store.
           Add your email below to stay up-to-date with announcements and our
           launch deals.
@@ -82,7 +82,7 @@ const App = () => {
 
         <form
           action="#"
-          className="w-[310px] flex justify-between items-center relative"
+          className="w-[310px] flex justify-between items-center relative md:w-[445px] md:h-[56px]"
           onSubmit={handleSubmit}
         >
           <div className="relative w-[100%]">
@@ -98,12 +98,12 @@ const App = () => {
               <img
                 src="../images/icon-error.svg"
                 alt="icon-error"
-                className="absolute right-2 top-1/2 transform -translate-y-1/2 w-[22px] h-[22px]"
+                className="absolute right-20 top-1/2 transform -translate-y-1/2 md:right-24"
               />
             )}
           </div>
 
-          <button className="bg-DesaturatedRed w-[65px] h-[44px] rounded-full flex justify-center items-center cursor-pointer absolute right-0">
+          <button className="bg-DesaturatedRed w-[65px] h-[44px] rounded-full flex justify-center items-center cursor-pointer absolute right-0 md:w-[85px]">
             <img
               src="../images/arrow_13050814.png"
               alt="icon-arrow"
